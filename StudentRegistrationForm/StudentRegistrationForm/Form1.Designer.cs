cc06b6974731c791cbd38d3a2c8e191055dbc16a
اسمك <dymhsn712@gmail.com>
2025-10-25 01:45:24 +0300

    Create Form1.Designer.cs

---
+++ StudentRegistrationForm/StudentRegistrationForm/Form1.Designer.cs
@@ -0,0 +1,41 @@
+﻿
+namespace StudentRegistrationForm
+{
+    partial class frmRegistration
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        private Button btnReset;
+        private PictureBox picStudent;
+        private Button btnUpload;
+        private Button btnSave;
+        private Button btnLoad;
+
+        // #endregion 
+
+
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+
+    }
+
+} 
+
